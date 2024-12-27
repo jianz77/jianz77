98c2da045688334df24d7743fda6929792620a27
jianz77 <1793956930@qq.com>
2024-12-27 16:44:58 +0800

    update postman 20241227

--- test/Postman.sh
+++ test/Postman.sh
@@ -1,44 +1,54 @@
-#为什么要做接口测试？
-接口的由来,连接前后端以及移动端。
-因为不同端的工作进度不一样，所以需要对开始出来的接口进行接口测试。
-#做接口测试的好处：
-1、节约时间,缩短项目成本
-2、提高工作效率
-3、提高系统的健壮性
-#Postman简介
-Postman是一个可扩展的API开发和测试协同平台工具，可以快速集成到CI/CD管道中。旨在简化测试和开发中的API工作流。
-Postman 工具有 Chrome 扩展和独立客户端，推荐安装独立客户端。
-Postman 有个 workspace 的概念，workspace 分 personal 和 team 类型。Personal workspace 只能自己查看的 API，Team workspace 可添加成员和设置成员权限，成员之间可共同管理 API。
 #如何下载安装Postman？
 https://www.postman.com/downloads/
-Postman的主要功能有请求、测试、环境、监视、历史、集合、工具、设置等。
-#如何使用Postman 举例一个请求
-#1. 打开Postman客户端
-#2. 点击左上角的 "New" 按钮，选择 "Request"
-#3. 在弹出的窗口中输入请求的名称和描述，然后点击 "Save to" 按钮选择保存的集合
-#4. 在请求窗口中选择请求类型（GET, POST, PUT, DELETE等）
-#5. 在URL输入框中输入请求的URL，例如：https://jsonplaceholder.typicode.com/posts
-#6. 如果是GET请求，直接点击 "Send" 按钮发送请求
-#7. 如果是POST请求，选择 "Body" 选项卡，选择 "raw" 并将类型设置为 "JSON"，然后在文本框中输入请求的JSON数据，例如：
-# {
-#   "title": "foo",
-#   "body": "bar",
-#   "userId": 1
-# }
-#8. 点击 "Send" 按钮发送请求
-#9. 在下方的响应窗口中查看请求的响应数据
 
+#如何使用Postman (实战 微信公众号平台)
+1 创建请求
+1.1 获得appid和secret
+ 打开微信公众平台网址https://mp.weixin.qq.com/
+ 找到接口测试号申请，进入微信公众账号测试号申请系统
+ 微信扫一扫，获取自己微信号的appID和appsecret
+ 微信公众平台API文档如下链接：
+ https://developers.weixin.qq.com/doc/offiaccount/Basic_Information/Get_access_token.html
 
+1.2 获取token值
+ https请求方式: GET 
+ https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=APPID&secret=APPSECRET
+ access_token 获取到的凭证
+ expires_in 凭证有效时间，单位：秒
 
-如何将请求参数化
-#1. 打开Postman客户端
-#2. 点击左上角的 "New" 按钮，选择 "Environment"
-#3. 在弹出的窗口中输入环境的名称和变量，然后点击 "Add" 按钮添加变量
-#4. 在请求窗口中使用 {{variable_name}} 的格式引用变量，例如：https://jsonplaceholder.typicode.com/posts/{{postId}}
-#5. 在发送请求前，选择环境，这样请求中的变量会被替换为环境中的实际值
-如何创建Postman Tests
+1.3 获取公众号已创建的标签接口
+ 输入url地址：https://api.weixin.qq.com/cgi-bin/tags/get
+ 参数为刚刚的access_token值， 
 
-如何创建测试集合
-如何使用Collection Runner 运行集合
-如何使用Newman运行集合
-面试的时候会问的问题：
+1.4 创建标签接口
+ 输入url地址：https://api.weixin.qq.com/cgi-bin/tags/create
+ 参数为刚刚的access_token值， 
+ 请求体为：在Body中，raw输入，以json格式输入，
+ {"tag":{"name":"test"}}
+    
+2 接口关联
+2.1 使用json提取器，提取access_token的值
+ 在Tests(scripts-post response)中首先查看响应的信息，然后使用json提取access_token参数以及其的值，最后将access_token的值设置为全局变量，
+ //打印responseBaby返回值
+ //console.log(responseBody)
+ //使用JSON提取结果,结果为键值对形式
+ //var result = JSON.parse(responseBody)
+ //access_token键中的值设置为全局变量
+ //pm.globals.set("access_token",result.access_token);
+
+ //解析响应体
+ var jsonResponse = pm.response.json();
+ //提取access_token
+ var access_token = jsonResponse.access_token;
+ //设置全局变量
+ pm.globals.set("access_token",access_token);
+ //可选：在控制台输出access_token,方便调试
+console.log("Access Token:",access_token);
+
+2.2 使用正则提取器，提取access_token的值
+ 上面为JavaScript脚本
+2.3 提取响应头的信息（Cookie……）
+2.4 从Cookie里面提取变量
+ //从cookie中提取变量
+ var contentType = pm.response.headers.get('Content-Type');
+
